678641c997c0c95514c2b283bec34dbeb7c54e95
V¡v€ķ Ğųřv€ <97387418+vivekgurve@users.noreply.github.com>
2024-06-22 23:33:25 +0530

    Add files via upload

---
+++ Examples/Input Pullup Serial/Input Pullup Serial Text Code.ino
@@ -0,0 +1,43 @@
+/*
+ Input Pullup Serial
+
+ This example demonstrates the use of pinMode(INPUT_PULLUP). It reads a
+ digital input on pin 2 and prints the results to the serial monitor.
+ 
+ OPEN THE SERIAL MONITOR TO SEE THE OUTPUT FROM THE INPUT PIN >>
+
+ The circuit:
+ * Momentary switch attached from pin 2 to ground
+ * Built-in LED on pin 13
+
+ Unlike pinMode(INPUT), there is no pull-down resistor necessary. An internal
+ 20K-ohm resistor is pulled to 5V. This configuration causes the input to
+ read HIGH when the switch is open, and LOW when it is closed.
+
+ */
+
+void setup() {
+  //start serial connection
+  Serial.begin(9600);
+  //configure pin2 as an input and enable the internal pull-up resistor
+  pinMode(2, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  //read the pushbutton value into a variable
+  int sensorVal = digitalRead(2);
+  //print out the value of the pushbutton
+  Serial.println(sensorVal);
+
+  // Keep in mind the pullup means the pushbutton's
+  // logic is inverted. It goes HIGH when it's open,
+  // and LOW when it's pressed. Turn on pin 13 when the
+  // button's pressed, and off when it's not:
+  if (sensorVal == HIGH) {
+    digitalWrite(LED_BUILTIN, LOW);
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+}
